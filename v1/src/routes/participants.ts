80ef8f0ea4967df3d13c4d5e3dfff59a5db9870d
Camel-app <camel.app@proton.me>
2023-10-11 13:17:28 +0200

    first commit

---
+++ v1/src/routes/participants.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const router = express.Router();
+
+import getOneExperimentToken from '../controllers/participant/getOneExperimentToken';
+import getOneExperiment from '../controllers/participant/getOneExperiment';
+import submitExperiment from '../controllers/participant/submitExperiment';
+
+const isAuth = require("../middlewares/auth");
+
+router.get('/getOneExperiment', getOneExperiment);
+router.post('/submitExperiment', isAuth, submitExperiment);
+router.get('/getOneExperimentToken', getOneExperimentToken);
+
+
+export default router;
+
+
